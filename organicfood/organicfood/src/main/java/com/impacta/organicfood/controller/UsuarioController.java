f620776ce4b82a4eb74a573a0f80462c262bd793
Andressa Ferreira <andressaffss@hotmail.com>
2023-10-16 23:22:48 -0300

    adicionando novas configuracoes de usuario

--- organicfood/organicfood/src/main/java/com/impacta/organicfood/controller/UsuarioController.java
+++ organicfood/organicfood/src/main/java/com/impacta/organicfood/controller/UsuarioController.java
@@ -1,33 +1,33 @@
-//package com.impacta.organicfood.controller;
-//
-//
-//import com.impacta.organicfood.model.Usuario;
-//import com.impacta.organicfood.model.UsuarioLogin;
-//import com.impacta.organicfood.service.UsuarioService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Optional;
-//@RestController
-//@RequestMapping("/usuario")
-//@CrossOrigin(origins = "*", allowedHeaders ="*")
-//public class UsuarioController {
-//	@Autowired
-//	private UsuarioService usuarioService;
-//	@PostMapping("/logar")
-//	public ResponseEntity<UsuarioLogin>Autentication(@RequestBody Optional <UsuarioLogin> user){
-//		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
-//				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
-//	}
-//	@PostMapping("/cadastrar")
-//	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-//		Optional<Usuario> user = Optional.ofNullable(usuarioService.CadastrarUsuario(usuario));
-//		try {
-//				return ResponseEntity.ok(user.get());
-//		} catch (Exception e) {
-//			return ResponseEntity.badRequest().build();
-//		}
-//	}
-//}
+package com.impacta.organicfood.controller;
+
+
+import com.impacta.organicfood.model.Usuario;
+import com.impacta.organicfood.model.UsuarioLogin;
+import com.impacta.organicfood.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders ="*")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+    @PostMapping("/logar")
+    public ResponseEntity<UsuarioLogin>Autentication(@RequestBody Optional <UsuarioLogin> user){
+        return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
+        Optional<Usuario> user = Optional.ofNullable(usuarioService.cadastrarUsuario(usuario));
+        try {
+            return ResponseEntity.ok(user.get());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
--- organicfood/organicfood/src/main/java/com/impacta/organicfood/service/UsuarioService.java
+++ organicfood/organicfood/src/main/java/com/impacta/organicfood/service/UsuarioService.java
@@ -1,56 +1,54 @@
-//package com.impacta.organicfood.service;
-//
-//
-//import com.impacta.organicfood.model.Usuario;
-//import com.impacta.organicfood.model.UsuarioLogin;
-//import com.impacta.organicfood.repository.UsuarioRepository;
-//import org.apache.tomcat.util.codec.binary.Base64;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import org.springframework.stereotype.Service;
-//
-//import java.nio.charset.Charset;
-//import java.util.Optional;
-//
-//@Service
-//public final class UsuarioService {
-//	@Autowired
-//	private UsuarioRepository repository;
-//
-//	public Usuario CadastrarUsuario(Usuario usuario) {
-//		if(repository.findAllByEmailContainingIgnoreCase(usuario.getEmail()).isPresent()) {
-//			return null;
-//		}
-//
-////		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//		String senhaEncoder = encoder.encode(usuario.getSenha());
-//		usuario.setSenha(senhaEncoder);
-//
-//		return repository.save(usuario);
-//	}
-//
-//	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user){
-//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//		Optional<Usuario> usuario = repository.findAllByEmailContainingIgnoreCase(user.get().getEmail());
-//
-//		if(usuario.isPresent()) {
-//			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
-//
-//			String auth = user.get().getEmail()+ ":" + user.get().getSenha();
-//			byte[] encondedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-//			String authHeader = "Basic "+ new String(encondedAuth);
-//			user.get().setToken(authHeader);
-//			user.get().setNome(usuario.get().getNome());
-//
-//			user.get().setSenha(usuario.get().getSenha());
-//			user.get().setTipoUsuario(usuario.get().getTipoUsuario());
-//
-//			return user;
-//
-//			}
-//		}
-//
-//		return null;
-//	}
-//}
+package com.impacta.organicfood.service;
+
+import com.impacta.organicfood.model.Usuario;
+import com.impacta.organicfood.model.UsuarioLogin;
+import com.impacta.organicfood.repository.UsuarioRepository;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+@Service
+public final class UsuarioService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        if (repository.findAllByEmailContainingIgnoreCase(usuario.getEmail()).isPresent()) {
+            return null;
+        }
+
+        Base64 encoder = new Base64();
+
+        // alterar para spring security
+        String senhaBase64 = new String(encoder.encode(usuario.getSenha().getBytes(Charset.forName("US-ASCII"))));
+        usuario.setSenha(senhaBase64);
+
+        return repository.save(usuario);
+    }
+
+    public Optional<UsuarioLogin> logar(Optional<UsuarioLogin> user) {
+        Base64 encoder = new Base64();
+        Optional<Usuario> usuario = repository.findAllByEmailContainingIgnoreCase(user.get().getEmail());
+
+        if (usuario.isPresent()) {
+            // Decodificar a senha do usuário
+            String senhaUsuario = new String(encoder.decode(usuario.get().getSenha().getBytes(Charset.forName("US-ASCII"))));
+
+            if (user.get().getSenha().equals(senhaUsuario)) {
+                String auth = user.get().getEmail() + ":" + user.get().getSenha();
+                byte[] encodedAuth = encoder.encode(auth.getBytes(Charset.forName("US-ASCII")));
+                String authHeader = "Basic " + new String(encodedAuth);
+                user.get().setToken(authHeader);
+                user.get().setNome(usuario.get().getNome());
+                user.get().setSenha(usuario.get().getSenha());
+                user.get().setTipoUsuario(usuario.get().getTipoUsuario());
+                return user;
+            }
+        }
+
+        return Optional.empty();
+    }
+}
